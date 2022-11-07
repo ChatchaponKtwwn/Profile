4ed063602a020479ea4cc461b926bb9adb5796b9
นายชัชชพล กิจทวีวาณิช <59059537+ChatchaponKtwwn@users.noreply.github.com>
2022-11-07 23:26:17 +0700

    Publish Web Playground with Github Pages

---
+++ js/app.js
@@ -0,0 +1,31 @@
+var app = angular.module("myModule", [])
+				.controller("myController", function($scope){
+				$scope.carts=[];
+				$scope.products = [
+					{p_id: "1", p_name: "Iphone 13", p_image: "images/1.jpg", p_price: 25000},
+					{p_id: "2", p_name: "iPhone 14 pro max", p_image: "images/2.jpg", p_price: 45870},
+
+				];
+				
+				$scope.add_cart = function(product){
+					if(product){
+						$scope.carts.push({p_id: product.p_id, p_name: product.p_name, p_price: product.p_price});
+					}	
+				}
+						
+					
+				$scope.total = 0;
+				
+				$scope.setTotals = function(cart){
+					if(cart){
+						$scope.total += cart.p_price;
+					}
+				}
+				
+				$scope.remove_cart = function(cart){
+					if(cart){
+						$scope.carts.splice($scope.carts.indexOf(cart), 1);
+						$scope.total -= cart.p_price;
+					}
+				}
+	});
